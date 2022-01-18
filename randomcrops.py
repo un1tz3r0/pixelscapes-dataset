1bf2744e475b82f749535b43852c08e3d1bd8e23
un1tz3r0 <un1tz3r0@gmail.com>
2022-01-18 17:09:46 -0500

    added outputting class-conditional dataset based on source image for improving gan training stability

--- randomcrops.py
+++ randomcrops.py
@@ -46,7 +46,7 @@ def weightedchoice(keyweights):
 		''' given a dictionary of keys and integer weights, choose a key with the probability of each key
 		being chosen proportional to it's weight. '''
 		from random import randint
-		totalweight = sum(keyweights.values())
+		totalweight = int(sum(keyweights.values()))
 		pos = randint(0, totalweight)
 		accum = 0
 		for key, weight in keyweights.items():
@@ -55,7 +55,7 @@ def weightedchoice(keyweights):
 				return key
 		return None
 
-def randomcrops(indir, outdir, outcount, outsize, statusinterval=100):
+def randomcrops(indir, outdir, outcount, outsize, weighting=1.0, withclasses=True, statusinterval=100):
 		''' prepare a dataset for stylegan training from source images
 
 			@param indir		the directory containing the source images
@@ -99,7 +99,11 @@ def randomcrops(indir, outdir, outcount, outsize, statusinterval=100):
 		print()
 		print(f"Skipped {skipped} of {total} images... weights for images based on area:")
 		totalweight = sum(sourceweights.values())
+		averageweight = totalweight/len(list(sourceweights.items()))
+		scaledweights = {}
 		for key, weight in sourceweights.items():
+			scaledweights[key] = averageweight + (weight - averageweight) * weighting
+		for key, weight in scaledweights.items():
 			print(f"  {weight/totalweight*100.0: 3.2g} {key}")
 		print()
 
@@ -108,16 +112,37 @@ def randomcrops(indir, outdir, outcount, outsize, statusinterval=100):
 			print(f"Creating non-existant output directory: {outdir}")
 			Path(outdir).mkdir(parents=True, exist_ok=False)
 			print()
-
+		else:
+			print(f"WARNING! Output directory exists, make sure this is what you want!")
+			print()
+		
 		# generate output images
+		
+		# build dicts of class number and numbered class output directory
+		if withclasses:
+			print(f"Creating classes for crops from each input image...")
+			classnums = {}
+			classdirs = {}
+			for classnum, key in enumerate(scaledweights.keys()):
+				classnums[key] = classnum
+				classdir = Path(outdir) / f"{classnum:05d}"
+				if not classdir.exists():
+					classdir.mkdir(parents=True, exist_ok=False)
+				else:
+					print("WARNING! Class output subdirectory exists!")
+				classdirs[key] = classdir
+
 		print(f"Generating random crops...")
 		for outnum in range(0, outcount):
-				sourceimfile = weightedchoice(sourceweights)
+				sourceimfile = weightedchoice(scaledweights)
 				sourceim = sourceims[sourceimfile]
 				sourcex = randint(0, sourceim.width - outsize)
 				sourcey = randint(0, sourceim.height - outsize)
 				outim = sourceim.crop((sourcex, sourcey, sourcex+outsize, sourcey+outsize))
-				outpath = Path(outdir) / f"{outnum:06d}.png"
+				if not withclasses:
+					outpath = Path(outdir) / f"{outnum:06d}.png"
+				else:
+					outpath = classdirs[sourceimfile] / f"{outnum:06d}.png"
 				outim.save(str(outpath))
 				if (outnum//statusinterval)*statusinterval == outnum:
 					print(f"\rSaved crop {outnum}/{outcount} at {sourcex}x{sourcey} from {sourceimfile} to {str(outpath)}\x1b[K", end="")
@@ -128,9 +153,12 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Generate randomly cropped squares from a set of source images for training GANs") 
 	parser.add_argument("sourcedir", type=str, help="The path to a directory containing the input images to crop")
 	parser.add_argument("destdir", type=str, help="The path to the output directory where the cropped images should be saved")
+	parser.add_argument("--no-classes", dest="withclasses", action="store_false", required=False, help="Do not generate output class directories based on which of the source images each training image was cropped from. Helps GANs avoid mode collapse and train faster.")
+	parser.add_argument("--classes", dest="withclasses", action="store_true", default=True, required=False, help="Generate output class directories based on which of the source images each training image was cropped from. Opposite of --no-classes, this is the default.")
+	parser.add_argument("--weighting", type=float, default=0.5, help="How much weighting to apply based on source image pixel count. Set to 0.0 for equal number of samples from each source image regardless of whether some source images are larger than others. 1.0 means weight by width * height, so a source with 500kpixels will be samples 5x as frequently as one with 100kpix. Intermediate values interpolate between even distribution and pixel-count-proportional weighting.")
 	parser.add_argument("--size", type=int, default=256, help="The size (width and height) of the square region to crop and output from the source images")
 	parser.add_argument("--count", type=int, default=50000, help="The number of randomly cropped output images to generate from the source images")
 	parser.add_argument("--status-interval", type=int, default=100, help="Print a progress message to stderr after each chunk of this many output images are written")
 	args = parser.parse_args()
-	randomcrops(args.sourcedir, args.destdir, args.count, args.size, args.status_interval)
+	randomcrops(args.sourcedir, args.destdir, args.count, args.size, weighting=args.weighting, withclasses=args.withclasses, statusinterval=args.status_interval)
 
